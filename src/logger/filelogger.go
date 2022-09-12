ae5c4e59dfc3dbe512ec6be74aa454044e87bd51
wisp <forworkandtravel@yandex.ru>
2022-09-12 10:13:36 +0000

    logger date prefix

--- src/logger/filelogger.go
+++ src/logger/filelogger.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"sync"
+	"time"
 )
 
 type FileLogger struct {
@@ -13,6 +14,10 @@ type FileLogger struct {
 
 	fh     *os.File
 	writer *bufio.Writer
+}
+
+func getTime() string {
+	return time.Now().Format("2006-01-02 15:04:05")
 }
 
 func NewFileLogger(fileName string) (logger *FileLogger, err error) {
@@ -36,20 +41,27 @@ func (l *FileLogger) Close() {
 func (l *FileLogger) Waring(msg ...interface{}) {
 	l.mtx.Lock()
 	defer l.mtx.Unlock()
-	fmt.Fprintln(l.writer, msg...)
+	fmt.Fprintln(l.writer, append([]interface{}{getTime()}, msg...)...)
 	l.writer.Flush()
 }
 
 func (l *FileLogger) Info(msg ...interface{}) {
 	l.mtx.Lock()
 	defer l.mtx.Unlock()
-	fmt.Fprintln(l.writer, msg...)
+	fmt.Fprintln(l.writer, append([]interface{}{getTime()}, msg...)...)
 	l.writer.Flush()
 }
 
 func (l *FileLogger) Error(msg ...interface{}) {
 	l.mtx.Lock()
 	defer l.mtx.Unlock()
-	fmt.Fprintln(l.writer, msg...)
+	fmt.Fprintln(l.writer, append([]interface{}{getTime()}, msg...)...) // fmt.Fprintln(l.writer, msg...)
 	l.writer.Flush()
+}
+
+func (l *FileLogger) Debug(msg ...interface{}) {
+	// l.mtx.Lock()
+	// defer l.mtx.Unlock()
+	// fmt.Fprintln(l.writer, append([]interface{}{getTime()}, msg...)...)
+	// l.writer.Flush()
 }
